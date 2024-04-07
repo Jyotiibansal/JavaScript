4a3850be35a3271200623ad8bcff63bd75af13d1
Jyoti Bansal <123709103+Jyotiibansal@users.noreply.github.com>
2024-04-07 16:39:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"  //treat all js as newer version
+ //we are using node.js not browser
+console.log(2+2);  console.log("hitesh")  //bad readibility 
+
+
+let name="jjjj";
+let age=18;
+let state=null;
+let city;
+let isLoggedIn=false; 
+// null =object 
+//null-standalone value
+//symbol =unique 
+console.log(typeof "jjjj");
+console.log(typeof "null");
