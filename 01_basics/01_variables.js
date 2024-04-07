162e08b93464b27f6a39d5d8a23e065d884db2ca
Jyoti Bansal <123709103+Jyotiibansal@users.noreply.github.com>
2024-04-07 16:21:11 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =14453
+let accountEmail ="bansalj2001@gmail.com"
+var accountPassword="bansal145@"
+accountCity="Jaipur"
+let accountstate; //undefined 
+
+//accountId=2;  //not allowed 
+accountEmail="hchv@gmail.com"
+accountPassword="bansal90@"
+accountCity="boston"
+// var is not used because of block scope and functional scope
+//use only let and const 
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
