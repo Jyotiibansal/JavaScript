091d0d192b5b8217f39aa2d9ac73fdeff4cf95df
Jyoti Bansal <123709103+Jyotiibansal@users.noreply.github.com>
2024-06-28 19:03:32 +0000

    chnges done.

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -5,3 +5,17 @@ console.log(typeof (score));
 
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
+
+
+
+
+//* operations *
+let value=3
+let neg_value=-value
+console.log(neg_value);
+
+
+let str1="hitesh"
+let str2="chaudhary"
+let str3=str1+str2
+console.log(str3);
