b6585f93475cf80fc811bbe7603ef9b9ddb8a067
Jyoti Bansal <123709103+Jyotiibansal@users.noreply.github.com>
2024-06-29 12:55:19 +0000

    done

---
+++ 01_basics/07_num.js
@@ -0,0 +1,11 @@
+const score=100
+console.log(score);
+
+const bal=new Number(100)
+console.log(bal);
+
+console.log(bal.toString());
+console.log(bal.toFixed(1));
+
+const othername=123.8966
+console.log(othername.toPrecision(3));
